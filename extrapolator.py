61bf49812035f41fceb734e7fe3ec541bab22c27
rhys <email.curly@gmail.com>
2019-04-29 16:36:40 +0100

    Added More Training Data

--- extrapolator.py
+++ extrapolator.py
@@ -8,13 +8,13 @@ class Extrapolator:
 		self.gen.train_from_file(
 				file, 
 				num_epochs=cfg["learn_times"],
-				gen_epochs=cfg["gen_amount"]
+				gen_epochs=cfg["test_amount"]
 			)
 		
 	def finetune_list(self, list_of_text, cfg, steps=1, gen=0):
 		self.gen.train_on_texts(list_of_text,
 				num_epochs=cfg["learn_times"],
-				gen_epochs=cfg["gen_amount"]
+				gen_epochs=cfg["test_amount"]
 			)
 
 	def save(self, out_file):
@@ -23,8 +23,8 @@ class Extrapolator:
 	def load(self, in_file):
 		self.gen = textgenrnn(in_file)
 
-	def generate(self, amount, randomness=1):
-		return self.gen.generate(amount, temperature=randomness, return_as_list=True)
+	def generate(self, amount, randomness=1, prefix=None):
+		return self.gen.generate(amount, temperature=randomness, return_as_list=True, prefix=prefix)
 
-	def generate_file(self, amount, out_file, randomness=1):
-		self.gen.generate_to_file(out_file, n=amount, temperature=randomness)
+	def generate_file(self, amount, out_file, randomness=1, prefix=None):
+		self.gen.generate_to_file(out_file, n=amount, temperature=randomness, prefix=prefix)
--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import extrapolator
 
 CFG = {
 	"randomness": 0.1,
-	"learn_times": 5,
-	"gen_amount": 20,
+	"learn_times": 1,
+	"gen_amount": 1,
+	"test_amount": 0 # generate while finetuning amount
 }
 
 if __name__ == '__main__':
 	e = extrapolator.Extrapolator()
 	
-	data = open("training_data.txt", "r").read().replace("\n", "").split("****")
+	data = open("training_data.txt", "r").read()\
+
+	data = data.replace("\n", "").split("****")
 
 	e.finetune_list(data, cfg=CFG)
 	
-	e.generate_file(CFG["gen_amount"], "out_data.txt", randomness=CFG["randomness"])
+	e.generate_file(CFG["gen_amount"], "out_data.txt", randomness=CFG["randomness"], prefix="Trump Thinks")
